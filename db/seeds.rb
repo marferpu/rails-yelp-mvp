7887de9962918bf7ea197bfb03ff700bdd5bbc6e
Marcela Fernandez <marferpu17@gmail.com>
2022-09-13 21:08:29 -0500

    avance

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.find_each(&:destroy)
+Restaurant.create(name: "Bigotes", address: "Carlos E 20-30", category: "italian")
+Restaurant.create(name: "Juanchos", address: "Aranjuez 20-30", category: "french")
+Restaurant.create(name: "Nutibara", address: "Centro 20-30", category: "chinese")
+Restaurant.create(name: "Bien chino", address: "Itagui 20-30", category: "chinese")
+Restaurant.create(name: "Montolivo", address: "Poblado 20-30", category: "italian")
+
+puts "Inserted in the db"
